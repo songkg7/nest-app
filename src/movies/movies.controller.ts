f2e4cb14c3be620fb3c5f9077f6240387a232508
Haril Song <songkg7@gmail.com>
2021-08-24 00:36:04 +0900

    NestJS 의 controller 사용

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll(): string {
+    return 'This will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie with made after: ${searchingYear}`;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: string) {
+    return `This will return one movie with the id: ${id}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
